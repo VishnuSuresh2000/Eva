d15b6f36c717ece8dcb63c882f062929bf53d37f
achu10vishnu <achu10vishnu@gmail.com>
2019-10-15 19:08:23 +0530

    Voice is activated

--- audioget.py
+++ audioget.py
@@ -1,21 +1,19 @@
 import speech_recognition as sr
-r=sr.Recognizer()
-mic_list=sr.Microphone.list_microphone_names()
 
-mic_name="Microphone (2- Realtek High Definition Audio)"
+microphone = sr.Microphone()
+recognizer = sr.Recognizer()
 
-for j,microphone_name in enumerate(mic_list):
-    if microphone_name == mic_name:
-        device_id = j
-with sr.Microphone(device_index = device_id,sample_rate=48000,chunk_size=1024) as source:
-    r.adjust_for_ambient_noise(source)
+
+with microphone as source:
+    recognizer.adjust_for_ambient_noise(source)
+    audio = recognizer.listen(source)
     print("Say Something")
     # listens for the user's input
-    audio = r.listen(source)
+    audio = recognizer .listen(source)
     print(audio)
 
     try:
-        text = r.recognize_google(audio)
+        text = recognizer.recognize_google(audio)
         print("you said: " + text)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
